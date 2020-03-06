22b59dd66fe3911d5f065096fb2989e470a81435
FYN_ fxy <FYN_ fxy@DESKTOP-L8T73B2.mshome.net>
2020-03-06 14:39:26 +0800

    清理注释 by方

--- ecms/src/main/java/com/ecms/core/config/InterceptorConfigure.java
+++ ecms/src/main/java/com/ecms/core/config/InterceptorConfigure.java
@@ -1,18 +1,4 @@
-/**
 
- *  TCMS2.0 版权声明<br/>
-
-    <center>Copyright (c) 2018 www.ecms.com</center> 
-
- <center> 2018年1月30日</center>
-
-<center>TCMS(ecms Content Management System)的所有资料（包括但不限于文字、图片、音频、视频资料及页面设计、排版、软件等）的版权和/或其他相关知识产权。</center>
-
-<center>未经桃李云事先书面许可,对本平台的任何内容、任何单位和个人不得以任何方式复制、转载、链接、转帖、引用、刊登、发表、反编译或者在非桃李云授权的所属服务器上做镜像或以其他任何方式使用。</center>
-
-<center>凡侵犯桃李云版权等知识产权的，桃李云将依法追究其法律责任。</center>
-
- */
 package com.ecms.core.config;
 
 import org.springframework.context.annotation.Configuration;
